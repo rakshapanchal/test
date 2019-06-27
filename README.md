bff0e9d58f10f7c82f058758ebf75a832f20eb8b
Raksha <raksha.panchal@dataeximit.com>
2019-06-27 19:19:03 +0530

    second commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# test-Project
+
+## Run backend 
+
+Run `nodemon` to run the project. which is running on port no:8085
+
+## Run frontend 
+
+Run `ng serve --open ` to run the project. which ll running on "http://localhost:4200"
--- public/angular-data/src/app/app.component.ts
+++ public/angular-data/src/app/app.component.ts
@@ -1,22 +1,23 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
-
 import { AppService } from './app.service';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  restaurantForm: FormGroup;
-  dishList: any
-  list: any
-  restaurantArray = [];
+  restaurantForm: FormGroup;//for formGroup name
+  dishList: any //for store dish data
+  list: any //for list of
+  sum = 0;
+  restaurantArray = [];//for store restaurant name
   title = 'angular-data';
   restaurantList: any
   constructor(private _formBuilder: FormBuilder,
-    private appService: AppService) { }
+    private appService: AppService,
+    private router: Router) { }
 
   get f() {
     return this.restaurantForm.controls;
@@ -26,16 +27,19 @@ export class AppComponent {
     this.buildRestaurantForm();
   }
 
+  /**for build form */
   buildRestaurantForm() {
     this.restaurantForm = this._formBuilder.group({
       meal: ['', [Validators.required]],
       people: ['1', [Validators.required, Validators.pattern(/^([1-9][0]*)$/)]],
       restaurantName: ['', Validators.required],
-      dishes: this._formBuilder.array([this._formBuilder.group({ dish: '', number: '1' })])
+      dishes: this._formBuilder.array([this._formBuilder.group({ dish: [''], number: ['1', [Validators.required, Validators.pattern(/^([1-9][0]*)$/)]] })])
     });
   }
 
+  /**for get all restaurant name */
   getRestaurant() {
+    localStorage.setItem('people', this.restaurantForm.controls['people'].value)
     this.appService.getRestaurants(this.restaurantForm.controls['meal'].value).subscribe((response: any) => {
       response.result.map((data) => {
         if (this.restaurantArray.indexOf(data.restaurant) == -1) {
@@ -50,6 +54,12 @@ export class AppComponent {
   }
 
   addDishes() {
+    this.dishes.push(this._formBuilder.group({ dish: '', number: '1' }));
+  }
+
+  array1 = [];
+
+  value(event) {
     this.restaurantForm.controls['dishes'].value.map((data) => {
       for (var i = 0; i < this.dishList.length; i++) {
         if (this.dishList[i].name === data.dish) {
@@ -57,7 +67,6 @@ export class AppComponent {
         }
       }
     })
-    this.dishes.push(this._formBuilder.group({ dish: '', number: '1' }));
   }
 
   deleteDishes(index) {
@@ -70,8 +79,19 @@ export class AppComponent {
     })
   }
 
-  finishFunction() {
-    console.log("lllll", this.restaurantForm.controls['dishes'].value)
+  getAllDish() {
+    this.restaurantForm.controls['dishes'].value.map((data) => {
+      this.sum += parseInt(data.number)
+    })
+    if (this.sum < parseInt(localStorage.getItem('people'))) {
+      alert("enter valid dish Number")
+      return;
+    }
     this.list = this.restaurantForm.controls['dishes'].value
   }
+
+  finish() {
+    location.reload(true);
+  }
+
 }
